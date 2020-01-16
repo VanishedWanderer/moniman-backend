cef55e1afd8055cded73c7e0d804648367d317c2
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2020-01-16 16:49:55 +0100

    Added KeyCloak

--- src/main/kotlin/at/vanishedwanderer/person/control/Person.kt
+++ src/main/kotlin/at/vanishedwanderer/person/control/Person.kt
@@ -2,6 +2,8 @@ package at.vanishedwanderer.person.control
 
 import at.vanishedwanderer.AdvancedPanacheRepository
 import io.quarkus.hibernate.orm.panache.PanacheRepository
+import io.quarkus.security.Authenticated
+import io.smallrye.jwt.auth.principal.JWTCallerPrincipal
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 import javax.persistence.Entity
@@ -12,8 +14,10 @@ import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
+import javax.ws.rs.core.Context
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
+import javax.ws.rs.core.SecurityContext
 
 @Path("/persons")
 @Produces(MediaType.APPLICATION_JSON)
@@ -35,6 +39,13 @@ class PersonEndpoint() {
         personDao.flush()
         personDao.refresh(person)
         return Response.ok(person).build()
+    }
+
+    @Path("me")
+    @Authenticated
+    fun getMe(@Context sec: SecurityContext): Response {
+        val principal = sec.userPrincipal as JWTCallerPrincipal
+        return Response.ok(principal.subject).build()
     }
 }
 
